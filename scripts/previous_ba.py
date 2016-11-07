465162cbaaafe42eeb4b00adeb2597ec09ba3dc4
poky <claire@poky.fr>
2016-11-07 20:24:42 +0100

    add next and previous fonctions +++

---
+++ scripts/previous_ba.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import subprocess
+try:
+    sys.path.append(os.environ['HOME_BA'])
+except:
+    print("error in HOME_BA environment variable")
+import pickle
+import env_variables
+import logging
+import logging.config
+logging.config.dictConfig(env_variables.LOGGING)
+
+
+def main():
+    save_file = os.path.join(env_variables.stopnextprevious_dir, env_variables.previous_file)
+    pickle.dump(True, open(previous_file, "wb" ))
+    logging.info('#### NEXT BANDE ANNONCE depuis bouton de telecommande')
+
+
+
+
+if __name__ == "__main__":
+    main()
