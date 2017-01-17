4c372b3ab9971c82e9897d5d00480bf953c8ffcc
poky <poky@myhome.net>
2017-01-17 21:38:50 +0100

    changement dans l'insertion des looped pour ne pas commencer et finir une playlist par le meme element

--- classes/insert/InsertSeveralTimes.py
+++ classes/insert/InsertSeveralTimes.py
@@ -13,15 +13,20 @@ class InsertSeveralTimes(IInsert):
 
     def insert(self, icontent, playlist):
         new_list = []
+        can_be_last = True
         if self.start_index == 0:
             new_list = [icontent]
+            can_be_last = False
 
         nb_trailer = 0
-        for each in playlist:
+        for index, each in enumerate(playlist):
             new_list.append(each)
             if isinstance(each, Trailer):
                 if nb_trailer != 0 and nb_trailer%self.loop_step == 0:
-                    new_list.append(icontent)
+                    if (index == len(playlist)-1 and can_be_last is False):
+                        pass
+                    else:
+                        new_list.append(icontent)
                 nb_trailer += 1
 
         return new_list
